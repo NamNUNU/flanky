91b7b16d44dc3203e300009f8c5f641bbd250e87
NamNUNU <aufcl4858@naver.com>
2018-02-13 00:18:58 +0900

    FK-7 타임 워치 기능 구현

--- src/screen/ExerciseScreen.tsx
+++ src/screen/ExerciseScreen.tsx
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 interface ExerciseScreenProps {
   navigation: any;
 }
 
-class ExerciseScreen extends Component<ExerciseScreenProps, {}> {
+interface ExerciseScreenState {
+  second: number;
+  isRunning: boolean;
+}
+
+class ExerciseScreen extends Component<
+  ExerciseScreenProps,
+  ExerciseScreenState
+> {
+  stopwatch: number;
   constructor(props) {
     super(props);
+    this.state = {
+      second: 0,
+      isRunning: false
+    };
+  }
+
+  _onPressButton() {
+    this.setState(
+      { isRunning: !this.state.isRunning },
+      this._onStartTime.bind(this)
+    );
+  }
+
+  _onStartTime() {
+    // 시계가 동작하지 않을 때 시작시킴
+    if (this.state.isRunning) {
+      this.stopwatch = setInterval(() => {
+        const second = this.state.second + 1;
+        this.setState({ second });
+      }, 1000);
+    } else {
+      // 시계가 동작하고 있을 때 정지 시킴
+      clearInterval(this.stopwatch);
+    }
   }
 
   render() {
     return (
       <View>
-        <Text>this is exercise Screen</Text>
+        <Text>{this.state.second}</Text>
+        <TouchableOpacity onPress={this._onPressButton.bind(this)}>
+          <Text>{this.state.isRunning ? 'Stop' : 'Start'}</Text>
+        </TouchableOpacity>
       </View>
     );
   }
