b427004f344fde84c8778ae507ec335dda880c7c
eunwoo <aufcl4858@naver.com>
2018-02-17 19:26:04 +0900

    FK-6 홈 뷰 스타일 적용

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 // model
-import { UserData } from '../common/Model'
+import { UserData } from '../common/Model';
 import { NavigationProps } from '../common/Model';
 import Exercise from '../common/Exercise';
 
@@ -16,14 +16,15 @@ import DayList from '../component/DayList';
 interface HomeScreenState {
   userData: UserData;
   exercisePlan: number[];
+  currentStep: number;
 }
 
 class HomeScreen extends Component<NavigationProps, HomeScreenState> {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      ...this.state,
-    }
+      ...this.state
+    };
   }
   componentDidMount() {
     // 유저 정보가 바뀔때마다 정보를 업데이트 시켜주는 리스너 추가
@@ -32,10 +33,14 @@ class HomeScreen extends Component<NavigationProps, HomeScreenState> {
 
   fetchedLocalItem(userData: UserData) {
     console.log('HomeScreen User Data:', userData);
-    if (userData.exerciseLevel === undefined) this.props.navigation.navigate(Router.SETTING);
+    if (userData.exerciseLevel === undefined)
+      this.props.navigation.navigate(Router.SETTING);
     else {
-      const exercisePlan = Exercise.getExercisePlan(userData.exerciseLevel);
-      this.setState({ ...this.state, userData, exercisePlan })
+      const exercisePlan: number[] = Exercise.getExercisePlan(
+        userData.exerciseLevel
+      );
+      const currentStep: number = 1;
+      this.setState({ ...this.state, currentStep, userData, exercisePlan });
     }
   }
 
@@ -44,27 +49,66 @@ class HomeScreen extends Component<NavigationProps, HomeScreenState> {
   }
 
   render() {
-    const { exercisePlan } = this.state;
-    return (
-      this.state.userData === undefined ?
-        <View>
-          <Text>you are first visitor!</Text>
-          <TouchableOpacity onPress={() => this.props.navigation.navigate(Router.SETTING)}>
-            <Text>Set up user level</Text>
-          </TouchableOpacity>
+    const { exercisePlan, currentStep } = this.state;
+    return this.state.userData === undefined ? (
+      <View>
+        <Text>you are first visitor!</Text>
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate(Router.SETTING)}
+        >
+          <Text>Set up user level</Text>
+        </TouchableOpacity>
+      </View>
+    ) : (
+      <View>
+        <View style={styles.planContainer}>
+          <View style={styles.planLeftBtn}>
+            <Text style={styles.leftBtnTxt}>{currentStep !== 0 && '<'}</Text>
+          </View>
+          <View style={styles.planText}>
+            <Text style={styles.dayTxt}>{currentStep}</Text>
+            <Text style={styles.second}>{exercisePlan[currentStep]}</Text>
+          </View>
+          <View style={styles.planRightBtn}>
+            <Text style={styles.rightBtnTxt}>
+              {currentStep !== exercisePlan.length && '>'}
+            </Text>
+          </View>
         </View>
-        :
-        <View>
-          <DayList
-            exercisePlan = {exercisePlan}
-          />
-          <Text>this is home view</Text>
-          <TouchableOpacity onPress={this._onPressButton.bind(this)}>
-            <Text>Exercise Start</Text>
-          </TouchableOpacity>
-        </View>
+        <Text>this is home view</Text>
+        <TouchableOpacity onPress={this._onPressButton.bind(this)}>
+          <Text>Exercise Start</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  planContainer: {
+    flexDirection: 'row'
+  },
+  planLeftBtn: {
+    flex: 1
+  },
+  planRightBtn: {
+    flex: 1
+  },
+  planText: {
+    flex: 4
+  },
+  leftBtnTxt: {
+    textAlign: 'center'
+  },
+  rightBtnTxt: {
+    textAlign: 'center'
+  },
+  dayTxt: {
+    textAlign: 'center'
+  },
+  second:{
+    textAlign: 'center'
+  }
+});
 
 export default HomeScreen;
