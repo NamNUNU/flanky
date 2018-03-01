238cca7cf0f3a1f4ae0c20ac91bfb63c08041328
eunwoo <aufcl4858@naver.com>
2018-03-01 23:59:46 +0900

    finish screen 스타일링

--- src/screen/FinishScreen.tsx
+++ src/screen/FinishScreen.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import { NavigationActions } from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 // model
 import Router from '../util/Router';
@@ -21,8 +22,17 @@ class FinishScreen extends Component<NavigationProps, {}> {
 
   render() {
     return (
-      <View>
-        <Text>exercise finish</Text>
+      <View style={styles.container}>
+        <View style={styles.titleWrap}>
+          <Text style={styles.title}>운동 종료</Text>
+        </View>
+        <View style={styles.resultImageWrap}>
+          <Icon name="trophy" size={120} color={'white'} />
+        </View>
+        <View  style={styles.descWrap}>
+          <Text style={styles.desc}>수고했어요</Text>
+          <Text style={styles.desc}>푹쉬고 내일도 열심히 운동해요!</Text>
+        </View>
         <TouchableOpacity
           style={CommonStyles.blueBtn}
           onPress={this._onPressComfirm.bind(this)}
@@ -33,5 +43,35 @@ class FinishScreen extends Component<NavigationProps, {}> {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#29a2d9',
+    padding: 20
+  },
+  titleWrap:{
+    paddingVertical:20,
+  },
+  title: {
+    fontSize: 32,
+    color: 'white',
+    textAlign:'center'
+  },
+  descWrap:{
+    paddingVertical:20,
+  },
+  desc: {
+    fontSize: 24,
+    color: 'white'
+  },
+  resultImageWrap: {
+    alignItems: 'center'
+  },
+  resultImage: {
+    width: 250,
+    height: 200
+  }
+});
 
 export default FinishScreen;
